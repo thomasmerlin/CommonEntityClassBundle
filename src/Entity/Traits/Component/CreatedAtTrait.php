a8f9df623fd5d38c25fe545369f0398e76678254
OuterAlchemy <contact.thomasmerlin@gmail.com>
2018-12-01 16:40:06 +0100

    Added a CreatedAtTrait

---
+++ src/Entity/Traits/Component/CreatedAtTrait.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Floaush\Bundle\CommonEntityClass\Entity\Traits\Component;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Trait CreatedAtTrait
+ * @package Floaush\Bundle\CommonEntityClass\Entity\Traits\Component
+ * @ORM\HasLifecycleCallbacks()
+ */
+trait CreatedAtTrait
+{
+    /**
+     * @ORM\Column(
+     *     name="createdAt",
+     *     type="datetime",
+     *     nullable=false,
+     *     unique=false
+     * )
+     *
+     * @var \DateTime
+     */
+    protected $createdAt;
+
+    /**
+     * @ORM\PrePersist()
+     *
+     * @return self
+     */
+    public function setCreatedAt(): self
+    {
+        $this->createdAt = new \DateTime('now');
+        return $this;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getCreatedAt(): \DateTime
+    {
+        return $this->createdAt;
+    }
+}
